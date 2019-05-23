5bc22a2599f3436241cdec9c02dcaf844604be9b
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 17:17:00 +0530

    Update 19.c

--- 19.c
+++ 19.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-  int a,b,i,v,num,r;
+  int a,b,i,r,v,num;
   scanf("%d %d",&a,&b);
     for(i=a+1;i<b;++i)
   {
@@ -9,7 +9,7 @@ int main()
       num=0;
       while(v!=0)
       {
-          rem=(v%10);
+          r=(v%10);
           num+=r*r*r;
          v/=10;
       }
